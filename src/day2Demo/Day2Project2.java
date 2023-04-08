d133a3bbc4bf3f00e14de216ea4334ff93cf5b38
nasim <nasim@10.0.0.10>
2023-04-08 02:05:34 -0400

    Implemented divide()

--- src/day2Demo/Day2Project2.java
+++ src/day2Demo/Day2Project2.java
@@ -4,6 +4,15 @@ public class Day2Project2 {
 	public static void main(String[] args) {
 		
 		System.out.println("day2 project2 HW");
+		int result = divide(4,2);
+		System.out.println(result);
+		
+		
+	}
+		public static int divide(int a, int b) {
+		int result= a/b;
+		return result;	
+		
 	}
 
 }
